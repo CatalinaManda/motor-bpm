9b4e6a1684b1c7d58143c5b29d98492dae6b0ab9
grossvater <gitpatch@gmail.com>
2020-03-10 10:14:35 +0200

    add dep to corda RPC

--- bpm/src/main/kotlin/com/ledgertech/motor/bpm/config/AttachmentsConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.ledgertech.motor.bpm.config
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ConfigurationProperties(prefix = "ledgertech.bpm")
-class AttachmentsConfig() {
-}
